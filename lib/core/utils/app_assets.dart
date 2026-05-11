fade1fbf71af75d35e76dd7887402357cdeec801
aakashchoudhary <aakash.choudhary@wiom.in>
2026-05-11 19:20:44 +0530

    assets added

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -2,4 +2,25 @@ abstract final class AppAssets {
   static const profilePhoto = 'assets/images/self.jpg';
   static const skillsSphere = 'assets/html/skills_sphere_animation.html';
   static const portfolioData = 'assets/data/portfolio_data.json';
+
+  // ADB Tool
+  static const adbAppsPage = 'assets/images/adb/adb_apps_page.png';
+  static const adbDevicesPage = 'assets/images/adb/adb_devices_page.png';
+  static const adbSettingsPage = 'assets/images/adb/adb_settings_page.png';
+  static const adbShellPage = 'assets/images/adb/adb_shell_page.png';
+  static const adbToolTip = 'assets/images/adb/adb_tool_tip.png';
+
+  // Cognix
+  static const cognixGame1 = 'assets/images/cognix/cognix_game_1.jpg';
+  static const cognixHome = 'assets/images/cognix/cognix_home.jpg';
+  static const cognixResult = 'assets/images/cognix/cognix_result.jpg';
+  static const cognixScore1 = 'assets/images/cognix/cognix_score_1.jpg';
+
+  // Mobile Mouse
+  static const mobilMouseAppConnect = 'assets/images/mobile_mouse/mobile_mouse_app_connect.jpg';
+  static const mobilMouseAppEvent1 = 'assets/images/mobile_mouse/mobile_mouse_app_event1.jpg';
+  static const mobilMouseAppEvent2 = 'assets/images/mobile_mouse/mobile_mouse_app_event_2.jpg';
+  static const mobilMouseAppHome = 'assets/images/mobile_mouse/mobile_mouse_app_home.jpg';
+  static const mobilMousePref = 'assets/images/mobile_mouse/mobile_mouse_pref.png';
+  static const mobilMouseToolTip = 'assets/images/mobile_mouse/mobile_mouse_tool_tip.png';
 }
